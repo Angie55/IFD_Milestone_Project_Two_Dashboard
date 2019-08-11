80a3bc53856f85f26f137a89fbc4d13d9a6edb68
EC2 Default User <ec2-user@ip-172-31-83-202.ec2.internal>
2019-08-11 16:41:12 +0000

    Added render labels to bar chart and chnaged angle of x axis labels. Added basic pie and donut charts to top section to style and develop further later

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -8,8 +8,12 @@ function makeGraphs(error, casualtyData) {
     // Top section number displays
     show_display_severity_percent(ndx, "Serious", "#percentage-incidents-serious");
     show_display_severity_percent(ndx, "Fatal", "#percentage-incidents-fatal");
+    // Pie and donut section
+    show_percentage_male_female(ndx);
+    show_day_of_week_most_incidents_occur(ndx)
+    // Bar chart of incidents by area
     show_incidents_per_area(ndx);
-    show_fatal_vs_serious_severity(ndx);
+    
     
     dc.renderAll();
 }    
@@ -50,35 +54,62 @@ function show_display_severity_percent(ndx, severity, element) {
         .group(severityPercent);
 }
 
+//  Pie chart to show males vs females casualties
+
+function show_percentage_male_female(ndx) {
+    var dim = ndx.dimension(dc.pluck('sex'))
+    var group = dim.group();
+    
+    dc.pieChart("#male_female_casualties")
+    .height(300) 
+    .radius(120)
+    .transitionDuration(500)
+    .dimension(dim)
+    .group(group);
+}
+
+//  Donut chart to show day of week most incidents occur
+function show_day_of_week_most_incidents_occur(ndx) {
+    var dim = ndx.dimension(dc.pluck('day_of_week'))
+    var group = dim.group();
+    
+    dc.pieChart("#incidents_day_of_week")
+    .height(300) 
+    .radius(120)
+    .innerRadius(50)
+    .transitionDuration(500)
+    .dimension(dim)
+    .group(group);
+}
+
+
+
+//  Bar chart to show number of incidents within bouroughs of London
 
 function show_incidents_per_area(ndx) {
       var dim = ndx.dimension(dc.pluck('local_authority'));
       var group = dim.group();
       
       dc.barChart("#incidents_per_area")
-          .width(1200)
+          .width(1100)
           .height(600)
-          .margins({top:10, right: 50, bottom: 30, left: 50})
+          .margins({top:30, right: 50, bottom: 80, left: 50})
           .dimension(dim)
           .group(group)
           .transitionDuration(500)
+          .renderlet(function (chart) {
+            chart.selectAll("g.x text")
+                .attr('transform', "rotate(-80)");
+           })
+          .renderLabel(true)
           .x(d3.scale.ordinal())
           .xUnits(dc.units.ordinal)
           .elasticY(true)
+          .yAxisLabel("No. of incidents")
           .xAxisLabel("Local Authority")
           .yAxis().ticks(30);
           
 }
 
-function show_fatal_vs_serious_severity(ndx) {
-    var dim = ndx.dimension(dc.pluck('severity_of_casualty'))
-    var group = dim.group();
-    
-    dc.pieChart("#fatal_serious_incidents")
-    .height(300) 
-    .radius(90)
-    .transitionDuration(500)
-    .dimension(dim)
-    .group(group);
-}
+
 

38088b5ebe8a93660230d8186d240e7bc02ddfb1
EC2 Default User <ec2-user@ip-172-31-83-202.ec2.internal>
2019-09-01 17:16:22 +0000

    Changed the middle pie chart into a donut chart and improved it's appearance with different labels and colours

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -63,7 +63,7 @@ function show_display_severity_percent(ndx, severity, element) {
 
 function show_percentage_male_female(ndx) {
     var pieColours = d3.scale.ordinal()
-        .range(['#0666f9', '#113B92']);
+        .range([ '#113B92', '#0666f9']);
     
     var dim = ndx.dimension(dc.pluck('sex'))
     var group = dim.group();
@@ -81,7 +81,7 @@ function show_percentage_male_female(ndx) {
         .colors(pieColours)
         .on('pretransition', function(chart) {
             chart.selectAll('text.pie-slice').text(function(d) {
-                return d.data.key + ': ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
+                return d.data.key + ': ' + Math.round((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
             });
         });
     
@@ -90,24 +90,23 @@ function show_percentage_male_female(ndx) {
 //  Pie chart of number of casualties in each age range
 function show_casualties_by_age(ndx) {
     
+        var pieColours = d3.scale.ordinal()
+        .range([ '#CBCBCB', '#B3DAF1', '#84A5B8' ]);
+    
         var ageDim = ndx.dimension(function(d) {
         switch (true) {
             case (d.age < 26):
-                return '13-25';
+                return 'Aged 13-25';
             case (d.age < 39):
-                return '26-38';
+                return 'Aged 26-38';
             case (d.age < 51):
-                return '39-50';
+                return 'Aged 39-50';
             case (d.age < 64):
-                return '51-63';
+                return 'Aged 51-63';
             case (d.age < 76):
-                return '64-75'; 
-            case (d.age < 88):
-                return '76-87';
-            case (d.age < 100):
-                return '88-99'; 
+                return 'Aged over 64'; 
             case (d.age == 'unknown'):
-                return 'unknown';    
+                return 'Unknown';    
            }
         });
     
@@ -115,13 +114,22 @@ function show_casualties_by_age(ndx) {
     
     
     dc.pieChart("#casualties_age_range")
-        .height(300) 
+        .height(400) 
         .radius(140)
         .transitionDuration(500)
+        .innerRadius(50)
         .dimension(ageDim)
-        .group(ageGroup);
-}
+        .group(ageGroup)
+        .externalLabels(30)
+        .drawPaths(true)
+        .colors(pieColours)
+        .on('pretransition', function(chart) {
+           chart.selectAll('text.pie-slice').text(function(d) {
+              return d.data.key + ': ' + Math.round((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
+        });
+    });
 
+}    
 
 //  Pie chart showing percentage of casualties in each class
 function show_percentage_casualty_class(ndx) {
@@ -144,7 +152,7 @@ function show_percentage_casualty_class(ndx) {
         .colors(pieColours)
         .on('pretransition', function(chart) {
             chart.selectAll('text.pie-slice').text(function(d) {
-                return d.data.key + ': ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
+                return d.data.key + ': ' + Math.round((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
             });
         });
 }
